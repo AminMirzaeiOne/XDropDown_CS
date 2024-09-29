07999fea42187eb52e3a09a7170db850c540c0c6
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-29 16:07:06 +0330

    - Create XToolStripDropDown Constractor Class

--- XDropDown/XToolStripDropDown.cs
+++ XDropDown/XToolStripDropDown.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,32 @@ namespace XDropDown
         private ToolStripControlHost m_host;
 
         private bool m_fade = true;
+
+        public XToolStripDropDown(System.Windows.Forms.Control popedControl)
+        {
+            InitializeComponent();
+            if (popedControl == null)
+                throw new ArgumentNullException("content");
+
+            this.m_popedContainer = popedControl;
+
+            this.m_fade = SystemInformation.IsMenuAnimationEnabled && SystemInformation.IsMenuFadeEnabled;
+
+            this.m_host = new ToolStripControlHost(popedControl);
+            m_host.AutoSize = false;//make it take the same room as the poped control
+
+            Padding = Margin = m_host.Padding = m_host.Margin = Padding.Empty;
+
+            popedControl.Location = Point.Empty;
+
+            this.Items.Add(m_host);
+
+            popedControl.Disposed += delegate (object sender, EventArgs e)
+            {
+                popedControl = null;
+                Dispose(true);// this popup container will be disposed immediately after disposion of the contained control
+            };
+        }
 
 
     }
